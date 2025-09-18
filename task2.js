fc5bc2a3a40026a4a01d35bb4aa58d8f0fd61a3e
Ivan Pan <auauauuau@gmail.com>
2025-09-18 03:43:30 -0700

    final version

--- task2.js
+++ task2.js
@@ -1,23 +1,5 @@
-/**Задача №2
 
-В программе объявлены две переменные — temp и weather. 
-Объявите переменную activity и присвойте ей значение по следующему правилу:
-если значение переменной temp больше либо равно 25 
-a переменная weather имеет значение "clear", 
-тогда значение переменной activity должно быть "golf"; 
-если значение переменной temp от 10 до 24 включительно или weather имеет значение "cloudy", 
-тогда значение переменной activity должно быть "bowling"; 
-в ином случае значение переменной activity должно быть "hiking".  
 
-Пример значений переменных: 
-
-let temp = 25 
-
-let weather = "clear"  
-
-Пример результата: 
-
-"golf" */
 const temp = Number(prompt("Temperature"));
 const weather = prompt("Weather (clear/cloudy)");
 if (isNaN(temp)) {
